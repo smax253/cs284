2661b9825ee350e0d7b18ea8cd6cfa183fb1a87c
Max Shi <smax253@gmail.com>
2019-04-05 22:18:37 -0400

    try again

--- src/hw3/MazeTest.java
+++ src/hw3/MazeTest.java
@@ -101,16 +101,16 @@ public class MazeTest extends JFrame implements GridColors {
     public void solve() {
         Maze m = new Maze(theGrid);
         //Uncomment this to test findMazePath
-//        boolean found = m.findMazePath();
-//        if (found) {
-//            JOptionPane.showMessageDialog(null, "Success - reset maze and try again");
-//        } else {
-//            JOptionPane.showMessageDialog(null, "No path - reset maze and try again");
-//        }
+        boolean found = m.findMazePath();
+        if (found) {
+            JOptionPane.showMessageDialog(null, "Success - reset maze and try again");
+        } else {
+            JOptionPane.showMessageDialog(null, "No path - reset maze and try again");
+        }
 
         //Uncomment this to test findAllMazePaths
-        ArrayList<ArrayList<PairInt>> results = m.findAllMazePaths(0,0);
-        JOptionPane.showMessageDialog(null, results);
+        //ArrayList<ArrayList<PairInt>> results = m.findAllMazePaths(0,0);
+        //JOptionPane.showMessageDialog(null, results);
 
         //Uncomment this to test findMazePathMin
         //ArrayList<PairInt> path = m.findMazePathMin(0, 0);
